153ec1f27e29cfece93aa526efbb72169f91225b
Poltroneando <poltroneando@gmail.com>
2017-09-13 20:36:19 -0300

    commit 13-09-17 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Poltroneando</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/login.css')}}">
+        {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+        <style>
+            body {
+                font-family: 'Helvetica';
+            }
+
+            .fa-btn {
+                margin-right: 6px;
+            }
+        </style>
+    </head>
+    <body id="app-layout">
+        <div class="topo-bar">
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="/imgs/marca.png" class="img-brand" alt="Poltroneando" style="max-height:35px;">
+            </a>
+        </div>
+        <div class="container">
+            <div class="white-box">
+                <h5>Desculpe, esta página não está disponível</h5>
+                <br>
+                <h5>O link acessado pode estar quebrado, ou a página pode ter sido removida.</h5>
+            </div>
+        </div>
+
+        <!-- JavaScripts -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+        {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    </body>
+</html>
