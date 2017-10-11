9238bfe1bc7f3ea6956efff9feb1e9a74f4ea432
Poltroneando <poltroneando@gmail.com>
2017-10-10 22:10:15 -0300

    commit 10-10-17 public_path

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{asset('public/css/login.css')}}"> 
+    <link rel="stylesheet" type="text/css" href="{{public_path('css/login.css')}}"> 
     <link rel="stylesheet" type="text/css" href="{{asset('public/css/menu.css')}}"> 
     {{-- <link href="{{ elixir('css/login.css') }}" rel="stylesheet"> --}}
 
