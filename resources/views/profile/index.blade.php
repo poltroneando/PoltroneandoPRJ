220dd7c9fd5dfa4c7a405c43442acecc6ec281ad
Poltroneando <poltroneando@gmail.com>
2017-10-22 13:17:29 -0200

    commit 22-10-17 imgborder

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -22,7 +22,7 @@
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-body">
-                <img src="{{ $user->foto }}">
+                <img src="{{ $user->foto }}" id="img-modal">
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
