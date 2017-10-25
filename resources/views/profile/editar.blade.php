2a756a2154c841a79c6010e8c1260dd4e7717ba8
Poltroneando <poltroneando@gmail.com>
2017-10-25 21:14:53 -0200

    commit 25-10-17 alert

--- resources/views/profile/editar.blade.php
+++ resources/views/profile/editar.blade.php
@@ -2,16 +2,38 @@
 
 @section('content')
 <div class="topo-perfil">
-    <div class="profile-capa" style="background-image:url('/uploads/covers/{{ $user->capa}}'">
-        <div class="profile-header">
-            <img src="{{ $user->foto }}" style="width:150px; height:150px; border-radius:10%;"></br>
-            <form enctype="multipart/form-data" action="/perfil" method="POST">
-                <label>Update Profile Image</label>
-                <input type="file" name="avatar">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="submit" class="pull-right btn btn-sm btn-primary">
-            </form>
+<div class="profile-capa" style="background-image:url('/uploads/covers/{{ $user->capa}}'">
+    <div class="profile-header">
+        <img src="{{ $user->foto }}" class="profile-pic" style="width:130px; height:130px; border-radius:10%;"></br>
+        <a class="profile-name">
+            <span class="blockLink">{{ $user->nome }}</span>
+        </a>
+    </div>
+</div>
+</div>
+<div class="container">
+    <div class="panel">
+        <div class="alert alert-danger">
+            <strong>Atenção!</strong> Complete as informações de seu perfil para que sua experiência no Poltroneando seja a melhor possível
         </div>
     </div>
+</div>
+<div class="modal fade" id="profile-picture" role="dialog">
+<div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-body">
+            <img src="{{ $user->foto }}" id="img-modal">
+        </div>
+        <div class="modal-footer">
+            <form enctype="multipart/form-data" action="/perfil" method="POST">
+                <input type="file" name="avatar">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="submit" class="btn btn-sm btn-primary">
+            </form>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>                
+        </div>
+    </div>
+</div>
 </div>
 @endsection
