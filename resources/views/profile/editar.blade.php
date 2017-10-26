a8c659219ff4040d07968b3348f850f7d2aa02a8
Poltroneando <poltroneando@gmail.com>
2017-10-25 22:05:49 -0200

    commit 25-10-17 form1

--- resources/views/profile/editar.blade.php
+++ resources/views/profile/editar.blade.php
@@ -21,8 +21,8 @@
     @endif
     <form class="form-horizontal" role="form" method="POST" action="{{ url('/gravar') }}">
         {{ csrf_field() }}
-        @if empty($user->password)
-       <!-- <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+        @if (empty($user->password))
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
             <label for="password" class="control-label sr-only">Senha</label>
             <div class="input-group">
                 <span class="input-group-addon" id="addon2"><span class="glyphicon glyphicon-lock"></span></span> 
@@ -33,7 +33,7 @@
                     <strong>{{ $errors->first('password') }}</strong>
                 </span>
             @endif
-       </div>-->
+       </div>
         @endif
         <button type="submit" class="btn btn-primary" style="background-color: #083C52;">
             Salvar Dados
