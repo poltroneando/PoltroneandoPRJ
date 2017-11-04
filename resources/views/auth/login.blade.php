7a2cda8d8ac00b0a1a299ff13c10f36010a3b679
Poltroneando <poltroneando@gmail.com>
2017-11-03 22:01:00 -0200

    commit 03-11-17 css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{asset('public/css/login.css')}}"> 
+    <link rel="stylesheet" type="text/css" href="{{asset('public/css/estilo.css')}}"> 
     <link rel="shortcut icon" href="{{{ asset('public/favicon.ico') }}}">
     {{-- <link href="{{ elixir('css/login.css') }}" rel="stylesheet"> --}}
 
