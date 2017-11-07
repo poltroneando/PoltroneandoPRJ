653ae66c777881b8fe079c57f34f846641c72301
Poltroneando <poltroneando@gmail.com>
2017-11-07 12:52:50 -0200

    commit 07-11-17 correcoes

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -8,11 +8,13 @@
         </div>
         @foreach ($news as $noticia)
         <div class="noticia">
-            <a href="">
+            <a href="{{$noticia->site_origem->url}}{{$noticia->url}}" target="_blank">
                 <img src="data:image/jpeg;base64,{{base64_encode($noticia->image)}}" alt="{{$noticia->title}}">
             </a>
             <p class="origem-noticia"><small>Por {{ $noticia->site_origem->name }} em {{$noticia->time_and_date}}</small></p>
-            <p class="titulo-noticia"><strong>{{ $noticia->title }}</strong></p> 
+            <a href="{{$noticia->site_origem->url}}{{$noticia->url}}" target="_blank">
+                <p class="titulo-noticia"><strong>{{ $noticia->title }}</strong></p> 
+            </a>
             @if (empty($noticia->subtitle) == 0) 
                 <p>{{$noticia->subtitle}}</p>
             @endif
