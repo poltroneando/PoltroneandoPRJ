a98ffa364b03a8c43ad16aa380d58218be632a72
Poltroneando <poltroneando@gmail.com>
2017-11-08 00:50:17 -0200

    commit 08-11-17 carbon

--- app/Noticia.php
+++ app/Noticia.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Noticia extends Model
 {
     public $table = 'news';
+    protected $dates = ['created_at', 'updated_at', 'time_and_date'];
+    protected $dateFormat = 'd-m-Y';
     public function site_origem()
     {
         return $this->belongsTo('App\Site_Origem','news_site_id');
