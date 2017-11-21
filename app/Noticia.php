76484b3890e0fa74e2333f4c33e6ecd3acd6fb5e
Poltroneando <poltroneando@gmail.com>
2017-11-21 18:10:30 -0200

    commit 21-11-17 noticias

--- app/Noticia.php
+++ app/Noticia.php
@@ -8,11 +8,11 @@ class Noticia extends Model
 {
     public $table = 'news';
     protected $dates = ['created_at', 'updated_at', 'time_and_date'];
-    protected $dateFormat = 'd-m-Y';
+    //protected $dateFormat = 'd-m-Y';
     public function site_origem()
     {
         return $this->belongsTo('App\Site_Origem','news_site_id');
-    }
+    }    
     public function categoria_noticia()
     {
         return $this->belongsTo('App\Categoria_noticia');
